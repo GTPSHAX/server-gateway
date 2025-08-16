9b4fb30ee930c7299251111d3d2c15d571402139
GTPSHAX <rafiehasannudin@gmail.com>
2025-08-16 00:12:53 -0700

    feat: Add missing files

--- loginurl/server.js
+++ loginurl/server.js
@@ -15,19 +15,27 @@ const handle = app.getRequestHandler();
 consola.log(`Running in ${dev ? 'development' : 'production'} mode`);
 
 const httpsOptions = {
-  key: fs.readFileSync(path.join(__dirname, 'cert', 'osm.growplus.asia-key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'cert', 'osm.growplus.asia-crt.pem')),
-  ca: fs.readFileSync(path.join(__dirname, 'cert', 'osm.growplus.asia-chain-only.pem'))
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'loginurl.growplus.asia-key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'loginurl.growplus.asia-crt.pem')),
+  ca: fs.readFileSync(path.join(__dirname, 'cert', 'loginurl.growplus.asia-chain-only.pem'))
 };
 
 app.prepare().then(() => {
-
   // Create HTTPS server
   createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen((dev ? 3000 : 3000), (err) => {
+  }).listen(3000, (err) => {
     if (err) throw err;
-    console.log(`> Ready on https://localhost${dev ? ':3000' : ':3000'} (${dev ? 'development' : 'production'} mode)`);
+    console.log(`> Ready on https://localhost:3000 (${dev ? 'development' : 'production'} mode)`);
+  });
+
+  // Create HTTP server (port 80)
+  http.createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(80, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:80 (${dev ? 'development' : 'production'} mode)`);
   });
 });
